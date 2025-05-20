aa83a4c0aef4edf33b744ef95ff74a8d3e2cf04d
amit kohli <amitkohli@macbookpro.lan>
2025-05-20 15:17:14 -0700

    Initial commit

---
+++ test/party-photo-backend-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PartyPhotoBackendCdk from '../lib/party-photo-backend-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/party-photo-backend-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PartyPhotoBackendCdk.PartyPhotoBackendCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
